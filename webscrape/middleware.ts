2369011e9729e5477d669b20776b5771ea1c4c74
tdogcodes <trxcycsgo@gmail.com>
2025-08-16 01:18:06 -0700

    changes in the landing page

--- webscrape/middleware.ts
+++ webscrape/middleware.ts
@@ -1,27 +1,20 @@
-import { getAuth } from "@clerk/nextjs/server";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export async function middleware(req: NextRequest) {
-  const publicPaths = ["/sign-in", "/sign-up", "/", "/api/workflows"];
+const isPublicRoute = createRouteMatcher([
+    "/sign-in(.*)", "/sign-up(.*)", "/api/workflows/(.*)*", "/",
+])
 
-  // Skip public paths
-  if (publicPaths.some(path => req.nextUrl.pathname.startsWith(path))) {
-    return NextResponse.next();
-  }
-
-  const { userId } = getAuth(req); // works in Node runtime
-
-  if (!userId) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-
-  return NextResponse.next();
-}
+export default clerkMiddleware((auth, request)=> {
+    if(!isPublicRoute(request)) {
+        auth().protect();
+    }
+})
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
+}
