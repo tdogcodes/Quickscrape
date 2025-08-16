3dde74ea9f471679e5d43ecddc129b5e5c8ea370
tdogcodes <trxcycsgo@gmail.com>
2025-08-16 01:31:52 -0700

    cleaned up middlewear

--- webscrape/middleware.ts
+++ webscrape/middleware.ts
@@ -1,14 +1,3 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isPublicRoute = createRouteMatcher([
-    "/sign-in(.*)", "/sign-up(.*)", "/api/workflows/(.*)*", "/",
-])
-
-export default clerkMiddleware((auth, request)=> {
-    if(!isPublicRoute(request)) {
-        auth().protect();
-    }
-})
 
 export const config = {
   matcher: [
