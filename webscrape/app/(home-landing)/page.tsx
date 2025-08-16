a6982fb3e3990a10a1beae89724140121bea120a
tdogcodes <trxcycsgo@gmail.com>
2025-08-16 00:06:09 -0700

    made the root dynamically rendered

--- webscrape/app/(home-landing)/page.tsx
+++ webscrape/app/(home-landing)/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import { TypewriterEffectSmooth } from "./_components/type-writer-effect";
 import { Button } from "@/components/ui/button";
